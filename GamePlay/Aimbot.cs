9ef423ba488c3071cf3f6ef0100d29f586c1ca41
silent developer <153481326+Adeell786@users.noreply.github.com>
2024-01-05 13:17:43 +0500

    Add files via upload

---
+++ GamePlay/Aimbot.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class Aimbot : MonoBehaviour
+{
+    private const string Tag = "Enemy";
+
+    public Transform playerCamera;
+    public float aimAssistRadius = 0.5f;
+    public float timeToAim = 0.3f;
+    public float speed = 1;
+
+    public void Start()
+    {
+        
+    }
+    public RotationChanges TrackTarget()
+    {
+        var target = SelectTarget();
+
+        // if we didn't find a target, just return an empty result that does not change the rotations
+        if (!target)
+        {
+            return RotationChanges.Empty;
+        }
+
+        var targetPos = target.position;
+        var totalHorizontalRotationAngles = CalculateTotalRotationAngles(Vector3.up, targetPos);
+        var totalVerticalRotationAngles = CalculateTotalRotationAngles(playerCamera.right, targetPos);
+
+        var dx = CalculateDeltaRotationDegrees(totalVerticalRotationAngles, timeToAim, Time.deltaTime*speed, targetPos);
+        var dy = CalculateDeltaRotationDegrees(totalHorizontalRotationAngles, timeToAim, Time.deltaTime*speed, targetPos);
+
+        // return the pitch addition in degrees and the turn addition in euler angles (rotated along Y axis)
+        return new RotationChanges(pitchAdditionInDegrees: dx, turnAddition: dy * Vector3.up);
+    }
+    public LayerMask Targetlayer;
+    private Transform SelectTarget()
+    {
+        // find start point and direction for the spherecast
+        var direction = playerCamera.transform.forward;
+        var startPoint = playerCamera.position;
+
+        // shoot the spherecast
+        if (Physics.SphereCast(startPoint, aimAssistRadius, direction, out var hit, 1000f,Targetlayer))
+        {
+            // if we didn't hit anything then we won't do any aimbotting
+            return hit.collider.transform;
+        }
+        else
+        {
+            return null;
+        }
+
+        // we found a target only if the collider we hit is tagged as 'enemy'
+        //return hit.collider.CompareTag(Tag) ? hit.collider.transform : null;
+    }
+
+    private float CalculateDeltaRotationDegrees(float totalRotation, float time, float deltaTime, Vector3 target)
+    {
+        // normalize for the aim assist radius or a short radius will have slower aim speed
+        var adjustedTimeToAim = time * aimAssistRadius;
+        
+        // get the distance from player to target
+        var distance = (target - playerCamera.transform.position).magnitude;
+        
+        // calculate the angle from its opposite and adjacent sides and divide by time to get angular velocity
+        var angularVelocity = Mathf.Atan2(1f, distance) * Mathf.Rad2Deg / adjustedTimeToAim;
+        
+        // to avoid overshoot, if we are closer to aiming at the target than one step, we just snap to it
+        return Mathf.Min(angularVelocity * deltaTime, Mathf.Abs(totalRotation)) * Mathf.Sign(totalRotation);
+    }
+
+    private float CalculateTotalRotationAngles(Vector3 planeNormal, Vector3 target)
+    {
+        // project to a plane defined by its normal vector, for both the actual and desired look directions
+        var camForwardProjected = Vector3.ProjectOnPlane(playerCamera.forward, planeNormal);
+        var playerToTargetProjected = Vector3.ProjectOnPlane((target - playerCamera.position).normalized, planeNormal);
+        
+        // calculate the signed angle between the actual and desired look directions
+        return Vector3.SignedAngle(camForwardProjected, playerToTargetProjected, planeNormal);
+    }
+}
---
+++ GamePlay/RotationChanges.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public struct RotationChanges
+{
+    public Vector3 TurnAddition { get; }
+        
+    public float PitchAdditionInDegrees { get; }
+
+    public RotationChanges(Vector3 turnAddition, float pitchAdditionInDegrees)
+    {
+        TurnAddition = turnAddition;
+        PitchAdditionInDegrees = pitchAdditionInDegrees;
+    }
+
+    public static RotationChanges Empty => new RotationChanges();
+}
